4a32fe412893164965b39f7b05f8aa500d7ce6c4
quaiyumlamisa <46520507+quaiyumlamisa@users.noreply.github.com>
2019-02-15 09:39:53 -0800

    basics

---
+++ mainwindow.cpp
@@ -0,0 +1,89 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog:: getOpenFileName(this, "Open the file");
+    QFile file(filename);
+    currentFile=filename;
+    if(!file.open(QIODevice::ReadOnly | QFile:: Text))
+    {
+         QMessageBox::warning(this,"Warning","Can't open file : " + file.errorString());
+
+    }
+    setWindowTitle(filename);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QString filename= QFileDialog::getSaveFileName(this,"save");
+    QFile file(filename);
+    if(!file.open(QFile::WriteOnly | QFile:: Text))
+    {
+
+        QMessageBox:: warning(this,"Warning" ,"Can't save file : " +file.errorString());
+        return;
+    }
+     currentFile=filename;
+      setWindowTitle(filename);
+       QTextStream out(&file);
+       QString text=ui->textEdit->toPlainText();
+       out<<text;
+       file.close();
+}
+
+
+
+
+
+
+x
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+   ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+     ui->textEdit->paste();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+     ui->textEdit->cut();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+     ui->textEdit->undo();
+}
